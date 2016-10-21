304891b3f7811ff0093192eba039cd1567498f0c
Daan Smits <Daan Smits>
2016-10-21 15:49:52 -0400

    Project setup

---
+++ web.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+import giphypop
+
+@app.route('/')
+def index():
+	search = request.values.get('search')
+	return render_template('index.html')
