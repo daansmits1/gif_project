3882700e16421ee56c8f4670c80e449cee33851f
Daan Smits <Daan Smits>
2016-10-24 17:21:40 -0400

    Initial try at getting the search function to work

--- web.py
+++ web.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 import giphypop
+g = giphypop.Giphy()
 
 @app.route('/')
 def index():
 	search = request.values.get('search')
 	return render_template('index.html')
+
+@app.route('/results')
+def results(): #needs to be right under the app line
+	results = g.search('search')
+	return render_template('results.html')
+
+@app.route('/about')
+def about(): #needs to be right under the app line
+	return render_template('about.html')
 
 app.run(debug=True)
